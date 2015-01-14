bf90c180a0d7f5093cce06d4fee8c0c6670778d4
Simon <hutch120@gmail.com>
2015-01-14 21:36:03 +1100

    Insert into elastic search with promise mechanism
    Insert into elastic search with promise mechanism

--- Scraper.js
+++ Scraper.js
@@ -1,254 +1,285 @@
+var Promise = require("bluebird");
 var traceback = require("traceback");
-var request = require('request');
+var rp = require('request-promise');
 var cheerio = require('cheerio');
+var elasticsearch = require('elasticsearch');
 
 'use strict'
 var scraper = new Scraper();
 scraper.setOptions();
 scraper.run();
 
+/**
+ * Scrape data from a web site.
+ *
+ * @class Scraper
+ */
 function Scraper() {
 
 	var _ = this;
-	
+
 	var testing;
 	_.testing = true;
-	
+
 	var opts;
 	_.opts = opts = {};
-	
+
 	var races;
 	_.races = races = {};
+
+	var esclient;
+	_.esclient = esclient;
 	
-	_.setOptions = function() {
-		
+	/**
+	 * Set options for the class.
+	 *
+	 * @method setOptions
+	 * @public
+	 */
+	_.setOptions = function () {
+
 		_.optsLive = {};
 		_.optsTest = {};
 
 		// URLs to scrape
 		_.optsTest.NeuralURL = 'http://localhost/neuraltest.html?raceid=';
 		_.optsTest.ResultsURL = 'http://localhost/resultstest.html?raceid=';
-		_.optsTest.ElasticSearchURL = 'http://localhost:9200/localhost/neural/';
-		
+		_.optsTest.ElasticSearchHost = 'localhost:9200';
+		_.optsTest.ElasticSearchIndex = 'localhost';
+		_.optsTest.ElasticSearchType = 'neural';
+
 		_.optsLive.NeuralURL = 'SEE LINKS';
 		_.optsLive.ResultsURL = 'SEE LINKS';
-		_.optsTest.ElasticSearchURL = _.optsTest.ElasticSearchURL;
-		
-		
-		if ( _.testing ) {
+		_.optsLive.ElasticSearchHost = _.optsTest.ElasticSearchHost;
+		_.optsLive.ElasticSearchIndex = _.optsTest.ElasticSearchIndex;
+		_.optsLive.ElasticSearchType = _.optsTest.ElasticSearchType;
+
+		_.esclient = new elasticsearch.Client({
+				host : _.optsLive.ElasticSearchHost,
+				log : 'error'
+			});
+
+		if (_.testing) {
 			_.opts = _.optsTest;
 		} else {
 			_.opts = _.optsLive;
 		}
-	
+
 	}
-	
+
+	/**
+	 * Entry point for the class.
+	 *
+	 * @method setOptions
+	 * @public
+	 */
 	_.run = function () {
 
 		var raceIDs = {
-			'547374': '' 
+			'547374' : '',
+			'547375' : ''
 		};
 
 		debugObject('raceIDs', raceIDs);
+
+		var getWebSiteData = [];
+		var insertWebSiteData = [];
 		
 		for (raceID in raceIDs) {
-			
+
 			debug('Parse raceID: ' + raceID);
-			
+
 			_.races[raceID] = {};
 			_.races[raceID].raceID = raceID;
-			_.parseNeuralData(raceID);
-		}		
+
+			_.races[raceID].data = {};
+			getWebSiteData.push(_.getNeuralData(raceID));
+
+			_.races[raceID].results = {};
+			getWebSiteData.push(_.getResultsData(raceID));
+			
+			insertWebSiteData.push(_.insertRace(raceID));
+		}
+		
+		Promise.all(getWebSiteData).then(function() {
+			debug("All the web site data has been retrived.");
+			
+			Promise.all(insertWebSiteData).then(function() {
+				debug("All the web site data has been inserted.");
+				_.esclient.close();
+			});			
+		});		
 	}
-	
-	/*	
-		See ./testpage/neuraltest.html for example.
-		
-		CP - Career performance assessment based on weight/class algorithms
-		CF - Current form measured by class/weight algorithms
-		TIM - Revolutionary time assessment (adjusted algorithm)
-		JA - Jockey ability algorithm
-		TA - Trainer ability algorithm
-		JT - Jockey/trainer combination algorithm
-		WT - Wet track performance algorithmCrs - Course suitability algorithm
-		D - Distance suitability algorithm$ - Prizemoney earned algorithm
-		BP - Barrier position (course & distance) algorithm
-		DLR - days since last run algorithm
-	*/
-	_.parseNeuralData = function (raceID) {
-		_.races[raceID].data = {};
-		var headers = {};
-		
+
+	/**
+	 * Parse Neural Data
+	 *
+	 * See ./testpage/neuraltest.html for example.
+	 *  CP - Career performance assessment based on weight/class algorithms
+	 *  CF - Current form measured by class/weight algorithms
+	 *  TIM - Revolutionary time assessment (adjusted algorithm)
+	 *  JA - Jockey ability algorithm
+	 *  TA - Trainer ability algorithm
+	 *  JT - Jockey/trainer combination algorithm
+	 *  WT - Wet track performance algorithmCrs - Course suitability algorithm
+	 *  D - Distance suitability algorithm$ - Prizemoney earned algorithm
+	 *  BP - Barrier position (course & distance) algorithm
+	 *  DLR - days since last run algorithm
+	 *
+	 * @method parseNeuralData
+	 * @public
+	 */
+	_.getNeuralData = function (raceID) {
 		var url = '' + _.opts.NeuralURL + raceID + '';
 		debug('URL: ' + url);
 		
-		request(url, function(error, response, html) {
-			if (error || response.statusCode != 200) {
-				throw error;
-			}
-			
-			var html = cheerio.load(html);
-			var table = cheerio.load(html('#offTblBdy2').parent().html());
-			
-			table('tr th').each(function(i, element){
-				var a = table(this);
-				//debug(i + ': ' + a.text());
-				headers[i] = a.text().trim();
-			});
-
-			//debugObject('headers', headers);
-
-			table('tbody tr').each(function(i, element){
-				var a = table(this);
-				_.races[raceID].data[i+1] = {};
-			
-				a.find('td').each(function(j, element){
-					var b = table(this);
-					//debug(i + ': ' + b.text());
-					//debug('headers[i]: ' + headers[i]);
-					
-					_.races[raceID].data[i+1][headers[j]] = b.text().trim();
-				});
-			});
-
-			_.neuralFinished(raceID);
-			
-		});	
-	
-	}
-	
-	_.neuralFinished = function (raceID) {
-		//debugObject('race', _.races);
-		_.parseResultsData(raceID);
-	}
-	
-	// 
-	_.parseResultsData = function (raceID) {
-		_.races[raceID].results = {};
-		var headers = {};
+		return rp({ url: url }).then(function (data) {
+			_.parseNeuralData(data, raceID);
+		})
+		.catch(function (err) {
+			debug("err.message: " + err.message);
+			debug("err.res.statusCode: " + err.res.statusCode);
+		});
 		
+	}
+
+	_.parseNeuralData = function (data, raceID) {
+		var headers = {};
+
+		var html = cheerio.load(data);
+		var table = cheerio.load(html('#offTblBdy2').parent().html());
+
+		table('tr th').each(function (i, element) {
+			var a = table(this);
+			//debug(i + ': ' + a.text());
+			headers[i] = a.text().trim();
+		});
+
+		//debugObject('headers', headers);
+
+		table('tbody tr').each(function (i, element) {
+			var a = table(this);
+			_.races[raceID].data[i + 1] = {};
+
+			a.find('td').each(function (j, element) {
+				var b = table(this);
+				//debug(i + ': ' + b.text());
+				//debug('headers[i]: ' + headers[i]);
+
+				_.races[raceID].data[i + 1][headers[j]] = b.text().trim();
+			});
+		});
+	}
+	
+	_.getResultsData = function (raceID) {
 		var url = '' + _.opts.ResultsURL + raceID + '';
 		debug('URL: ' + url);
-		
-		request(url, function(error, response, html) {
-			if (error || response.statusCode != 200) {
-				throw error;
+
+		return rp({ url: url }).then(function (data) {
+			_.parseResultsData(data, raceID);
+		})
+		.catch(function (err) {
+			debug("err.message: " + err.message);
+			debug("err.res.statusCode: " + err.res.statusCode);
+		});		
+	}
+
+	_.parseResultsData = function (data, raceID) {
+	
+		var headers = {};
+		var html = cheerio.load(data);
+		var table = cheerio.load(html('table .normbold').parent().html());
+		var skipRow = false;
+		var rowIndex = 0;
+
+		table('tr').each(function (row, element) {
+			var a = table(this);
+
+			if (a.text().trim() == '') {
+				skipRow = true;
+			} else {
+				//debug('row: ' + a.text().trim()	);
+				skipRow = false;
+				_.races[raceID].results[rowIndex] = {};
+
 			}
-			
-			var html = cheerio.load(html);
-			var table = cheerio.load(html('table .normbold').parent().html());
-			var skipRow = false;
-			var rowIndex = 0;
-			
-			table('tr').each(function(row, element){
-				var a = table(this);
-				
-				if ( a.text().trim() == '' ) {
+
+			a.find('td').each(function (col, element) {
+				var tablecell = table(this);
+
+				//debug('b.text().trim(): ' + b.text().trim());
+				if (row == 0) {
+					headers[col] = tablecell.text().trim();
 					skipRow = true;
 				} else {
-					//debug('row: ' + a.text().trim()	);
-					skipRow = false;
-					_.races[raceID].results[rowIndex] = {};
-
-				}
-				
-				a.find('td').each(function(col, element){
-					var tablecell = table(this);
-					
-					//debug('b.text().trim(): ' + b.text().trim());
-					if ( row == 0 ) {
-						headers[col] = tablecell.text().trim();
+					if (skipRow || (col == 0 && tablecell.text().trim() == '')) {
 						skipRow = true;
 					} else {
-						if ( skipRow || (col == 0 && tablecell.text().trim() == '')) {
-							skipRow = true;
-						} else {
-							//debug('rowIndex ' + rowIndex + ' col ' + col + ' text ' + tablecell.text().trim() + ' headers[col]: ' + headers[col]);
-							_.races[raceID].results[rowIndex][headers[col]] = tablecell.text().trim()
-							
-						}
+						//debug('rowIndex ' + rowIndex + ' col ' + col + ' text ' + tablecell.text().trim() + ' headers[col]: ' + headers[col]);
+						_.races[raceID].results[rowIndex][headers[col]] = tablecell.text().trim()
+
 					}
-				});
-				
-				if ( !skipRow ) {
-					rowIndex = rowIndex + 1;
-					//debug('increment rowIndex to ' + rowIndex);
 				}
 			});
 
-			//debugObject('headers', headers);
-			//debugObject('results', _.races[raceID].results);
-			
-			// Insert place into data for easier retrieval.
-			for (var key in _.races[raceID].results) {
-				
-				var tab = _.races[raceID].results[key].TAB;
-				var fp = _.races[raceID].results[key].FP;
-				//debug('tab: ' + tab);
-				_.races[raceID].data[tab]['FP'] = fp;
+			if (!skipRow) {
+				rowIndex = rowIndex + 1;
+				//debug('increment rowIndex to ' + rowIndex);
 			}
-			
-			_.resultsFinished(raceID);
-			
-		});	
-	
+		});
+
+		//debugObject('headers', headers);
+		//debugObject('results', _.races[raceID].results);
+
 	}
-	
-	_.resultsFinished = function (raceID) {
-		//debugObject('race', _.races[raceID]);
-		_.insertRace(raceID);
-	}
-	
+
 	_.insertRace = function (raceID) {
-	
-		var url = _.opts.ElasticSearchURL + raceID + '/_update';
 
-        _.esDSL = '{ "doc" : ' + JSON.stringify(_.races[raceID]) + ', "doc_as_upsert": true}';
+		// Insert place into data for easier retrieval.
+		for (var key in _.races[raceID].results) {
 
-		debug(url);
-        debug(_.esDSL);
-
-		var options = {
-		  method: 'post',
-		  json: true,
-		  url: url,
-		  body: _.esDSL,
+			var tab = _.races[raceID].results[key].TAB;
+			var fp = _.races[raceID].results[key].FP;
+			//debug('tab: ' + tab);
+			_.races[raceID].data[tab]['FP'] = fp;
 		}
+	
+		//var url = _.opts.ElasticSearchURL + raceID + '/_update';
 
-		debugObject('options', options);
-		
-		// TODO: This causes elastic search to go 100% CPU! 
+		//_.esDSL = '{ "doc" : ' + JSON.stringify(_.races[raceID]) + ', "doc_as_upsert": true}';
+
+		//debug(url);
+		//debug(_.esDSL);
+
+		// TODO: This causes elastic search to go 100% CPU!
 		// Tested DSL code on _plugin/head, and insert working so JSON format correct.
-		/*
-		request(options, function (err, res, body) {
-		  if (err) {
-			debugObject('error posting json', err)
-		  } else {
-			  var headers = res.headers
-			  var statusCode = res.statusCode
-			  debugObject('headers', headers)
-			  debugObject('statusCode', statusCode)
-			  debugObject('body', body)
-		  }
-		})*/
+		_.esclient.index({
+			index : _.opts.ElasticSearchIndex,
+			type : _.opts.ElasticSearchType,
+			id : raceID,
+			body : JSON.stringify(_.races[raceID])
+		}, function (error, response) {
+			if ( typeof error !== 'undefined' ) {
+				debugObject('error', error);
+			}
+			debugObject('response', response);
+		});
 
 	}
-
 }
-
-
 
 var lastMessageNoNewLine = false;
 
 function debug(msg) {
-	if ( lastMessageNoNewLine ) {
+	if (lastMessageNoNewLine) {
 		console.log('\n');
 		lastMessageNoNewLine = false;
 	}
 
 	var d = new Date();
-	var tb = traceback()[1]; // 1 because 0 should be your enterLog-Function itself
-	console.log(d.toJSON() + ' ' + tb.file + ':' + tb.line + ':\t' + msg);
+	// var tb = traceback()[1]; // 1 because 0 should be your enterLog-Function itself
+	// console.log(d.toJSON() + ' ' + tb.file + ':' + tb.line + ':\t' + msg);
+	console.log(d.toJSON() + ':\t' + msg);
 }
 
 function debugObject(msg, obj) {
@@ -258,4 +289,9 @@ function debugObject(msg, obj) {
 function debugNoNewline(msg) {
 	lastMessageNoNewLine = true;
 	process.stdout.write(msg);
+}
+
+// For request promise handling.
+function clientError(e) {
+    return e.code >= 400 && e.code < 500;
 }
